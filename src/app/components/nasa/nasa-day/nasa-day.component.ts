eafc01a7f4a28427ac2505ee1ce9adbb5718016d
LordPedroFelipe <pf.souza15@gmail.com>
2020-09-16 11:27:11 -0300

    #000 - Adiconando consumo da API da Nasa, projeto APOD

--- src/app/components/nasa/nasa-day/nasa-day.component.ts
+++ src/app/components/nasa/nasa-day/nasa-day.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NasaApodService } from '../nasa-apod.service';
 import { NasaApod } from '../nasa-apod.model';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-nasa-day',
@@ -10,19 +11,33 @@ import { NasaApod } from '../nasa-apod.model';
 export class NasaDayComponent implements OnInit {
 
   nasaApod: NasaApod;
+  date = new FormControl(new Date());
 
   constructor(
     private nasaApodService: NasaApodService
   ) { }
 
   ngOnInit(): void {
-    this.getNasaApodDay();
+    this.getNasaApodDay(null);
   }
 
-  getNasaApodDay(): void {
-    this.nasaApodService.read().subscribe(retorno => {
-      console.log(retorno);
-      this.nasaApod = retorno;
-    });
+  getNasaApodDay(date): void {
+    if (date === null) {
+      this.nasaApodService.read().subscribe(retorno => {
+        console.log(retorno);
+        this.nasaApod = retorno;
+      });
+    } else {
+      this.nasaApodService.readByDate(date).subscribe(retorno => {
+        console.log(retorno);
+        this.nasaApod = retorno;
+      });
+    }
+  }
+
+  changeDate(event): void {
+    const date = event.value;
+    const formattedDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    this.getNasaApodDay(formattedDate);
   }
 }
